c57aacc8a87256bae10f82f2ab039b4c7a81d13e
Moshe <mobgadol@gmail.com>
2022-05-25 17:14:42 -0400

    added an event listener for the form

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-var btnClick = function (){
-    var taskList = document.querySelector(".task-list");
-    var newItem = document.createElement("li");
-    newItem.textContent = "New task";
-    newItem.className = "task-item";
-    taskList.appendChild(newItem);
-};
-buttonEl.addEventListener("click", btnClick);
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
