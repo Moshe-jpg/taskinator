a35c6c8ad87e1894ad48a6d7cbeba1dfd4494192
Moshe <mobgadol@gmail.com>
2022-05-26 15:11:38 -0400

    added a reset feature a conditional statement to validate tha t the user is submitting an input

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+      }
+    formEl.reset();
+
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
